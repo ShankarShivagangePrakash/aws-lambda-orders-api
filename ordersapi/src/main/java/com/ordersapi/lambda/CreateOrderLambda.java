972d8eb31cd854738c1e88310b7c1dc034b00dcf
sshivagangeprakash <Shankar.ShivagangePrakash@logmein.com>
2023-11-11 23:28:58 +0530

    dynamo db integration

--- ordersapi/src/main/java/com/ordersapi/lambda/CreateOrderLambda.java
+++ ordersapi/src/main/java/com/ordersapi/lambda/CreateOrderLambda.java
@@ -1,5 +1,9 @@
 package com.ordersapi.lambda;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -18,6 +22,14 @@ public class CreateOrderLambda {
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+
+        DynamoDB dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.defaultClient());
+        Table table = dynamoDB.getTable(System.getenv("ORDERS_TABLE"));
+        Item item = new Item()
+                .withPrimaryKey("id", order.getId())
+                .withString("itemName", order.getItemName())
+                .withInt("qty", order.getQty());
+        table.putItem(item);
         return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody("order id: " + order.getId());
     }
 }
--- ordersapi/src/main/java/com/ordersapi/lambda/ReadOrderLambda.java
+++ ordersapi/src/main/java/com/ordersapi/lambda/ReadOrderLambda.java
@@ -1,5 +1,11 @@
 package com.ordersapi.lambda;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -7,10 +13,23 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ordersapi.model.Order;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ReadOrderLambda {
     public APIGatewayProxyResponseEvent getOrder(APIGatewayProxyRequestEvent request) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
+
+        AmazonDynamoDB dynamoDB = AmazonDynamoDBClientBuilder.defaultClient();
+        ScanResult scanResult = dynamoDB.scan(new ScanRequest().withTableName(System.getenv("ORDERS_TABLE")));
+        List<Order> orders = scanResult.getItems().stream()
+                    .map(item -> new Order(
+                            Integer.parseInt(item.get("id").getN()),
+                            item.get("itemName").getS(),
+                            Integer.parseInt(item.get("qty").getN())
+                    )).collect(Collectors.toList());
+
         return new APIGatewayProxyResponseEvent().withStatusCode(200)
-                .withBody(objectMapper.writeValueAsString(new Order(123, "Mac Book Pro", 3)));
+                .withBody(objectMapper.writeValueAsString(orders));
     }
 }
